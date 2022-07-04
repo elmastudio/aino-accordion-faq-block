52d33332d9d086982ca4216d8584eac7e0f4393b
Ellen Bauer <ellen.bauer@elmastudio.de>
2022-07-04 21:07:17 +1200

    1.1.2 update

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '941adae80e02afc14deffa2d8e20ac88');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'af05d658158519c76fa35d78d3634f5d');
--- src/edit.js
+++ src/edit.js
@@ -22,15 +22,7 @@ import {
 } from '@wordpress/components';
 import './index.scss';
 
-/**
- * The edit function describes the structure of your block in the context of the
- * editor. This represents what the editor will render when the block is used.
- *
- * @see https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#edit
- *
- * @return {WPElement} Element to render.
- */
-export default function Edit({ attributes, setAttributes, className, onSelect }) {
+export default function Edit({ attributes, setAttributes, className }) {
 
 	const {
 		question,
